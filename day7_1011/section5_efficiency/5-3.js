9ae33c973fdddd38862c1c146e4db96765bc79dd
saywhat <95282021+loveflora@users.noreply.github.com>
2023-10-04 19:05:49 +0900

    feat: 섹션 5 추가

---
+++ day7_1011/section5_efficiency/5-3.js
@@ -0,0 +1,19 @@
+//=== 5-3. 연속부분수열 1 ===
+
+function solution(m, arr) {
+  let answer = 0,
+    lt = 0,
+    sum = 0;
+  for (let rt = 0; rt < arr.length; rt++) {
+    sum += arr[rt];
+    if (sum === m) answer++;
+    while (sum >= m) {
+      sum -= arr[lt++];
+      if (sum === m) answer++;
+    }
+  }
+  return answer;
+}
+
+let a = [1, 2, 1, 3, 1, 1, 1, 2];
+console.log(solution(6, a));
